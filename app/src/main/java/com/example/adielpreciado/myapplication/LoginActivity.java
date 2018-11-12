a626e1d7cf7e77dbdbdbf6f888670b49ddc33f5a
Adiel Preciado <adielpreciado@upeu.edu.pe>
2018-11-12 12:26:48 -0500

    new

--- app/src/main/java/com/example/adielpreciado/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/adielpreciado/myapplication/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.adielpreciado.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,11 +16,28 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
         edtusu = (EditText) findViewById(R.id.Usuario);
         edtclv = (EditText) findViewById(R.id.Clave);
         btningr = (Button) findViewById(R.id.btningr);
         register = (TextView) findViewById(R.id.txtRH);
+
+    }
+
+    public void onClick(View v) {
+        Intent miIntent = null;
+        switch (v.getId()) {
+            case R.id.txtRH:
+                miIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                break;
+        }
+        if (miIntent != null) {
+            startActivity(miIntent);
+        }
     }
 }
+
+
+
+
 
